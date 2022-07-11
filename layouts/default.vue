d8e1e699810a8917e341959fbbf8d0e595c2e822
Alberto Romero <aromeronavia@gmail.com>
2022-07-10 20:45:11 -0500

    refinements of UI

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-screen h-screen bg-[#161438] text-white">
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,50 @@
 <template>
-  <div class="w-screen px-4 py-6">
+  <div
+    class="w-80 px-4 py-6 bg-[#161438] text-white border border-white rounded-3xl"
+  >
     <section class="flex justify-between mb-8">
       <div class="flex-col">
-        <p>Volume</p>
-        <button class="block" @click="volumeUp">Up</button>
-        <button class="block" @click="volumeDown">Down</button>
+        <p class="text-xl font-medium">Volume</p>
+        <button
+          class="block bg-blue-800 p-2 w-16 h-12 rounded-md mt-1"
+          @click="volumeUp"
+        >
+          Up
+        </button>
+        <button
+          class="block bg-blue-800 p-2 w-16 h-12 rounded-md mt-2"
+          @click="volumeDown"
+        >
+          Down
+        </button>
       </div>
-      <button @click="enter">Enter</button>
+      <button class="" @click="enter">Enter</button>
       <div class="flex-col">
-        <button class="block">Spotify</button>
-        <button class="block" @click="openYoutube">Youtube</button>
+        <p class="text-xl font-medium">Apps</p>
+        <button
+          class="block bg-green-800 p-2 h-12 rounded mt-1 active:bg-green-900 active:outline outline-1 w-full"
+        >
+          Spotify
+        </button>
+        <button
+          class="block bg-red-700 p-2 h-12 w-full rounded mt-2 active:bg-red-900 active:outline outline-1"
+          @click="openYoutube"
+        >
+          Youtube
+        </button>
       </div>
     </section>
     <footer>
-      <p>Lofi</p>
-      <div class="flex-wrap">
-        <button @click="openLofi">Lofi channel 1</button>
+      <p class="text-xl font-medium">Saved</p>
+      <div class="flex-col gap gap-1">
         <button
-          class="p-1 text-white bg-purple-600 rounded"
+          class="block p-1 text-white bg-purple-600 rounded mt-2"
+          @click="openLofi"
+        >
+          Lofi channel 1
+        </button>
+        <button
+          class="block p-1 text-white bg-purple-600 rounded mt-2"
           @click="openTransistorOst"
         >
           Transistor OST
